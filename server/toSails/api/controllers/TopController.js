5f0ee3da77433c19d163fb090568c48a7558edfa
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-07-03 13:08:12 +0900

    認証が通っているのなら、カレンダーの情報も取れるはず

--- server/toSails/api/controllers/TopController.js
+++ server/toSails/api/controllers/TopController.js
@@ -53,7 +53,15 @@ module.exports = {
 
     getUser(req, res, function(user){
       if(!googleAuthenticate(res, user, oauth2Client)){
-        res.view("top");
+        oauth2Client.getToken(user.googleAccessToken, function(err, token) {
+          if (err) {
+            console.log('Error while trying to retrieve access token', err);
+            return;
+          }
+          oauth2Client.credentials = token;
+          listEvents(oauth2Client);
+          res.view("top");
+        });
       }
     });
   },
@@ -72,26 +80,31 @@ module.exports = {
   },
 };
 
-var getNewToken = function(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
+var listEvents = function(auth) {
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    console.log(response);
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
       }
-      oauth2Client.credentials = token;
-      console.log(token);
-      callback(oauth2Client);
-    });
+    }
   });
 }
