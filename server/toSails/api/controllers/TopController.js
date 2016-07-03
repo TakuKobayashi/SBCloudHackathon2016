a407a1b21ac0cbbe4c3182097f25cd88d5ea8aa1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-07-03 12:47:43 +0900

    こんな感じにやってみた

--- server/toSails/api/controllers/TopController.js
+++ server/toSails/api/controllers/TopController.js
@@ -12,13 +12,27 @@ var getAuth2Client = function(req){
   return new auth.OAuth2(googleApiInfo.clientId, googleApiInfo.clientSecret, redirectUrl);
 }
 
-var createUser = function(res,callback){
-  var uuid = require('node-uuid');
-  var token = uuid.v4();
-  User.create({token: token}).exec(function(err, user){
-    res.cookie(host, user.token);
-    callback(user);
-  });
+var getUser = function(req, res,callback){
+  var host = req.headers.host;
+  var createUser = function(r, cb){
+  	var uuid = require('node-uuid');
+    var token = uuid.v4();
+    User.create({token: token}).exec(function(err, user){
+      r.cookie(host, user.token);
+      cb(user);
+    });
+  }
+  if(req.cookies[host]){
+    User.findOne({token: req.cookies[host]}).exec(function(err, user){
+      if(user){
+        callback(user);
+      }else{
+        createUser(res, callback);
+      }
+    });
+  }else{
+    createUser(res, callback);
+  }
 }
 
 var googleAuthenticate = function(res, user, oauth2Client){
@@ -36,33 +50,25 @@ var googleAuthenticate = function(res, user, oauth2Client){
 module.exports = {
   index: function (req,res) {
     var oauth2Client = getAuth2Client(req);
-    var host = req.headers.host;
 
-    if(req.cookies[host]){
-      User.findOne({token: req.cookies[host]}).exec(function(err, user){
-        if(user){
-          if(!googleAuthenticate(res, user, oauth2Client)){
-            res.view("top");
-          }
-        }else{
-          createUser(res, function(user){
-            if(!googleAuthenticate(res, user, oauth2Client)){
-              res.view("top");
-            }
-          });
-        }
-      });
-    }else{
-      createUser(res, function(user){
-      	googleAuthenticate(res, user, oauth2Client);
+    getUser(req, res, function(user){
+      if(!googleAuthenticate(res, user, oauth2Client)){
         res.view("top");
-      });
-    }
+      }
+    });
   },
 
   googleauth: function (req,res) {
-    console.log(req.params);
-    return res.redirect('http://47.88.138.17:1337/top');
+    console.log(req.param("code"));
+    return res.redirect('http://47.88.138.17:1337/top/redirectauth?code=' + req.param("code"));
+  },
+
+  redirectauth: function (req,res) {
+    getUser(req, res, function(user){
+      user.googleAccessToken = req.param("code");
+      user.save()
+      return res.redirect('/top');
+    })
   },
 };
 
